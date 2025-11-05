ed35d6244834e241c62d40460eb99576a965c688
Aditya Pujari <app63@njit.edu>
2025-11-04 20:45:54 -0600

    (refactor): mask after setting the output

--- dist/index.js
+++ dist/index.js
@@ -27562,8 +27562,8 @@ async function run() {
     console.log("Hello, GitHub Action");
     const uuid = randomUUID();
     console.log("Generated UUID:", uuid);
-    // core.setSecret(uuid);
     core.setOutput('token', uuid);
+    core.setSecret(uuid);
 }
 
 run().catch(err => core.setFailed(err.message));
