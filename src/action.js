e6553afd613a1e79db895645791833a20f2896b6
Aditya Pujari <app63@njit.edu>
2025-11-04 20:25:58 -0600

    (feat): using masked token

--- src/action.js
+++ src/action.js
@@ -1,5 +1,12 @@
+const core = require('@actions/core');
+const { randomUUID } = require('crypto');
+
 async function run() {
     console.log("Hello, GitHub Action");
+    const uuid = randomUUID();
+    console.log("Generated UUID:", uuid);
+    core.setSecret(uuid);
+    core.setOutput('token', uuid);
 }
 
-run();
+run().catch(err => core.setFailed(err.message));
